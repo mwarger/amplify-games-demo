6e72a70f3f313b4e3ecbad90b0b1c5d0f3c8d940
Mat Warger <mwarger@gmail.com>
2019-09-02 14:48:34 -0500

    updates

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,7 +27,7 @@ const awsmobile = {
     "https://7ansecvq2jcajkaz4jhl25vone.appsync-api.us-east-1.amazonaws.com/graphql",
   aws_appsync_region: "us-east-1",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-pf6hxm4xknc3tepuphiwkuv36q",
+  aws_appsync_apiKey: "da2-qxgbhpu5dnfhlmrhfvisezahgm",
   aws_mobile_analytics_app_id: "f7dc8e2658d942bf841fb085479f7eb4",
   aws_mobile_analytics_app_region: "us-east-1"
 }
